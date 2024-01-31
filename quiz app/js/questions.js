0fbaf1d463495f316eb38d530b270b378cec21fd
vcrutuja <142998263+vcrutuja@users.noreply.github.com>
2024-01-31 21:26:38 +0530

    Add files via upload

---
+++ quiz app/js/questions.js
@@ -0,0 +1,115 @@
+// creating an array and passing the number ,questions options and answers
+
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper tool markup language",
+            "Hyper Text Multiple Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "Cascade Styling Sheet",
+        options: [
+            "Common style Sheet",
+            "Colorful style Sheet",
+            "Computer style Sheet",
+            "Cascade Styling Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "Hypertext PreProcessor",
+        options: [
+            "Hypertext PreProcessor",
+            "Hypertext Programming",
+            "Hypertext PreProgramming",
+            "Hometext PreProcessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+            "Structured Query Language",
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "Extensible markup language",
+        options: [
+            "Extensible markup language",
+            "Executable Multiple Language",
+            "Extra multiprogram language",
+            "Examine Multiple Language"
+        ]
+    },
+    {
+        numb: 6,
+        question: "We enclose HTML tags within?",
+        answer: "<>",
+        options: [
+            "( )",
+            "{ }",
+            "<>",
+            "None of above"
+        ]
+    },
+    {
+        numb: 7,
+        question: "How to create an ordered list in HTML?",
+        answer: "ol",
+        options: [
+            "ul",
+            "ol",
+            "href",
+            "b"
+        ]
+    },
+    {
+        numb: 8,
+        question: "How many sizes of headers are available in HTML by default?",
+        answer: "6",
+        options: [
+            "5",
+            "1",
+            "3",
+            "6"
+        ]
+    },
+    {
+        numb: 9,
+        question: "How can we change the background color of an element?",
+        answer: "Background-color",
+        options: [
+            "Background-color",
+            "Color",
+            "Both A and B",
+            "None of above"
+        ]
+    },
+    {
+        numb: 10,
+        question: "In how many ways can CSS be written in?",
+        answer: "3",
+        options: [
+            "1",
+            "2",
+            "3",
+            "4"
+        ]
+    },
+
+];
